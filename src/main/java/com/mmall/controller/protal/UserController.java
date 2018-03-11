f0dd9eb56751b32c0ebd645963822d4229df807d
shiguan <1274870534@qq.com>
2018-03-11 19:10:43 +0800

    登陆模块

---
+++ src/main/java/com/mmall/controller/protal/UserController.java
@@ -0,0 +1,42 @@
+package com.mmall.controller.protal;
+
+import com.mmall.common.Constants;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Created by 15M-4528S on 2018/3/11.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    /**
+     * 用户登陆
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value="/login",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username,password);
+        if(response.isSuccess()){
+           session.setAttribute(Constants.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+}
